49d93d4ef1740e7102f908a8b4f1746d745045f5
nicolas <nicolascirulli2@gmail.com>
2026-05-13 12:51:58 -0300

    feat: fetchCityById en servicio de ciudades

--- src/features/cities/services/cities.services.ts
+++ src/features/cities/services/cities.services.ts
@@ -1,8 +1,12 @@
-import API from "@shared/api/api"
+import API from "@shared/api/api";
 import type { AxiosCityResponse, City } from "../cities.types";
 
 export const fetchCities = async (): Promise<City[]> => {
-    const response:AxiosCityResponse = await API.get("/cities")
-    console.log(response);
-    return response.data;
-}
+  const response: AxiosCityResponse = await API.get("/cities");
+  return response.data;
+};
+
+export const fetchCityById = async (id: string): Promise<City> => {
+  const response = await API.get(`/cities/${id}`);
+  return response.data;
+};
