118c341ad0863a68784bcb45b5311747409d8d09
nicolas <nicolascirulli2@gmail.com>
2026-05-13 12:51:42 -0300

    feat: rutas /register y /forgot-password en Router

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from "react-router";
 import MainLayout from "@shared/components/MainLayout";
 import HomePage from "@pages/home/HomePage";
 import CitiesPage from "@pages/cities/CitiesPage";
-
+import CityDetailsPage from "@pages/cities/CityDetailsPage";
+import LoginPage from "@/pages/login/LoginPage";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,13 @@ export const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/cities", element: <CitiesPage /> },
+      { path: "/cities/:id", element: <CityDetailsPage /> },
+      { path: "/login", element: <LoginPage /> },
     ],
+  },
+  {
+    path: "*",
+    element: <MainLayout />,
+    children: [{ path: "*", element: <h1>404 Not Found</h1> }],
   },
 ]);
